028380232a8eb3744678147860a9320fedd3ae84
Lukas Gräßle <Lukas.Graessle@web.de>
2016-05-14 17:32:57 +0200

    fix(crud): delete crud service

--- src/app/domain/issue.service.ts
+++ src/app/domain/issue.service.ts
@@ -1,12 +1,54 @@
 import {Injectable} from '@angular/core';
-import CrudService from './crud.service';
+import {Http} from '@angular/http';
+import Factory from './factory';
+import {AuthenticationService} from './../shared/authentication.service';
 
 @Injectable()
-export class IssueService extends CrudService {
+export class IssueService {
+  private owner:String;
+  private type:string = 'issue';
+  private apiUrl:String = "https://api.github.com";
+  private factory:Factory;
   
-  constructor() {
-    super('issue');
-  }
-  
+  constructor(private http: Http, private authenticationService:AuthenticationService) {}
 
+  public get(repoName:string) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}`;
+    return this.http.get(url, {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
+
+  public getOne(repoName:string, number:any) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}/${number}`;
+    return this.http.get(url, {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
+
+  public create(repoName:string, obj:any) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}`;
+    return this.http.post(url, obj.toJson(), {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
+
+  public update(repoName:string, number:any, obj:any) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}/${number}`;
+    return this.http.patch(url, obj.toJson(), {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
+
+  public delete(repoName:string, number:any) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}/${number}`;
+    return this.http.delete(url, {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
 }
--- src/app/domain/milestone.service.ts
+++ src/app/domain/milestone.service.ts
@@ -1,11 +1,57 @@
 import {Injectable} from '@angular/core';
-import CrudService from './crud.service';
+import {Http} from '@angular/http';
+import Factory from './factory';
+import {AuthenticationService} from './../shared/authentication.service';
 
 @Injectable()
-export class MilestoneService extends CrudService {
-  
-  constructor() {
+export class MilestoneService {
+  private owner:String;
+  private type:string = 'issue';
+  private apiUrl:String = "https://api.github.com";
+  private factory:Factory;
+
+  constructor(private http:Http, private authenticationService:AuthenticationService) {
     super('milestone');
+  }
+
+  public get(repoName:string) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}`;
+    return this.http.get(url, {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
+
+  public getOne(repoName:string, number:any) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}/${number}`;
+    return this.http.get(url, {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
+
+  public create(repoName:string, obj:any) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}`;
+    return this.http.post(url, obj.toJson(), {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
+
+  public update(repoName:string, number:any, obj:any) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}/${number}`;
+    return this.http.patch(url, obj.toJson(), {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
+  }
+
+  public delete(repoName:string, number:any) {
+    const url = `${this.apiUrl}/repos/${this.owner}/${repoName}/${this.type}/${number}`;
+    return this.http.delete(url, {
+      headers: this.authenticationService.getAuthedHeader()
+    })
+      .map(res => this.factory.translate(this.type, res.json()));
   }
 
 }
