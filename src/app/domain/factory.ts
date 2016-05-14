c41dc02098ff8c357b09cca982c93097414fe73f
Michael Hoppe <michael0.hoppe@googlemail.com>
2016-05-14 13:09:34 +0200

    fix(factory): fix domainObjects creation

--- src/app/domain/factory.ts
+++ src/app/domain/factory.ts
@@ -23,17 +23,24 @@ export class Factory {
       return user;
     }):User;
 
-    domainObject = type === Repository.toString() ? data.map(r =>
-      new Repository(r.id, r.name, r.full_name, r.url)) : 'error';
+    domainObject = type === Repository.toString() ? data.map(r => {
+      return new Repository()
+    }):Repository;
 
-    domainObject = type === Milestone.toString() ? data.map(r =>
-      new Milestone(r.number, r.id, r.titel, r.description, r.open_issues, r.closed_issues)) : 'error';
 
-    domainObject = type === Label.toString() ? data.map(r =>
-      new Label(r.name, r.color, r.url)) : 'error';
+    domainObject = type === Milestone.toString() ? data.map(r => {
+      return new Milestone();
+    }):Milestone;
 
-    domainObject = type === Issue.toString() ? data.map(r =>
-      new Issue(r.number, r.id, r.state, r.titel, r.body, r.milestone.id, r.labels)) : 'error';
+
+    domainObject = type === Label.toString() ? data.map(r => {
+      return new Label();
+    }):Label;
+
+
+    domainObject = type === Issue.toString() ? data.map(r => {
+      return new Issue()
+    }):Issue;
 
 
     return domainObject;
