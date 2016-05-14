4743a912d50b50567ad927d068182ebbc4e27b23
Stephan Koeninger <github@stekoe.de>
2016-05-14 15:26:50 +0200

    chore(footer): add footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-footer',
+  templateUrl: 'footer.component.html',
+  styleUrls: ['footer.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+})
+export class FooterComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
