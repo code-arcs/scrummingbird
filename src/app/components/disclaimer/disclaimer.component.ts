24cad8ec21314892d1dc47c2881fda0cac356a21
Stephan Koeninger <github@stekoe.de>
2016-05-14 15:27:08 +0200

    chore(disclaimer): added disclaimer component

---
+++ src/app/components/disclaimer/disclaimer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-disclaimer',
+  templateUrl: 'disclaimer.component.html',
+  styleUrls: ['disclaimer.component.css']
+})
+export class DisclaimerComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/disclaimer/index.ts
@@ -0,0 +1,1 @@
+export { DisclaimerComponent } from './disclaimer.component';
