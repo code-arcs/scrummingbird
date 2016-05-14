3835640adb54a337b84f5212463f475f65fac991
sprengerjo <sprengerj22@gmail.com>
2016-05-14 15:16:39 +0200

    chore(tests): minor testing of factory translation

--- src/app/domain/factory.spec.ts
+++ src/app/domain/factory.spec.ts
@@ -11,8 +11,13 @@ describe('factory translation', () => {
   beforeEachProviders(() => [Factory]);
 
   it('should translate repository',
-    inject([Factory], (fac:Factory) => {
+    inject([Factory], (fac: Factory) => {
       expect(fac).toBeTruthy();
     }));
 
+  it('should return null for invalid params',
+    inject([Factory], (fac: Factory) => {
+      expect(fac.translate('lizard', null)).toBeNull();
+      expect(fac.translate('lizard', [1, 2])).toBeNull();
+    }));
 });
